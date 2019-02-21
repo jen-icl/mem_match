20e241a75e07a13c3dee581b13372c5e6b5d5307
I Chun Lai <jenniferlai.81@gmail.com>
2019-02-21 00:22:36 -0800

    added images, updated main.js

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ $(document).ready(initializeApp);
 function initializeApp(){
     console.log('ready');
     create_card();
-    $("#game-area").on('click', '.card', card_clicked);
+    $(".card").on('click', card_clicked);
 }
 
 var first_card_clicked = null;
@@ -11,56 +11,58 @@ var second_card_clicked = null;
 var first_card_details = {}; //###return value of class and imgsource for comparison
 var second_card_details = {};
 var wait_timeout = false;
-var total_possible_matches = 2; //full game = 9
+var total_possible_matches = 9; //full game = 9
 var match_counter = 0;
 
-/*
-<div class="card happy1">
-    <div class="front">
-        <img src="images/Cat-Happy.png" alt="image-front">
-    </div>
-    <div class="back">
-        <img src="images/Cat-Back.png" alt="image-back">
-    </div>
-</div>
- */
 var game_images = [
-    ['images/Cat-Happy.png', 'happy'],
-    ['images/Cat-Sad.png', 'sad'],
-    ['images/Cat-Happy.png', 'happy'],
-    ['images/Cat-Sad.png', 'sad'],
-    ['images/Cat-Happy.png', 'happy'],
-    ['images/Cat-Sad.png', 'sad'],
-    ['images/Cat-Happy.png', 'happy'],
-    ['images/Cat-Sad.png', 'sad'],
-    ['images/Cat-Happy.png', 'happy'],
-    ['images/Cat-Sad.png', 'sad'],
-    ['images/Cat-Happy.png', 'happy'],
-    ['images/Cat-Sad.png', 'sad'],
-    ['images/Cat-Happy.png', 'happy'],
-    ['images/Cat-Sad.png', 'sad'],
-    ['images/Cat-Happy.png', 'happy'],
-    ['images/Cat-Sad.png', 'sad'],
-    ['images/Cat-Happy.png', 'happy'],
-    ['images/Cat-Sad.png', 'sad'],
+    ['images/mercury.png', 'mercury'],
+    ['images/venus.png', 'venus'],
+    ['images/earth.png', 'earth'],
+    ['images/mars.png', 'mars'],
+    ['images/jupiter.png', 'jupiter'],
+    ['images/saturn.png', 'saturn'],
+    ['images/uranus.png', 'uranus'],
+    ['images/neptune.png', 'neptune'],
+    ['images/pluto-sphere.png', 'pluto-sphere']
+]; //image source list
 
-]; //image source code list
+function random_array(array){
+    var current_index = array.length;
+
+    while(0 !== current_index){
+        var random_index = Math.floor(Math.random() * current_index);
+        current_index--;
+        var temp = array[current_index];
+        array[current_index] = array[random_index];
+        array[random_index] = temp;
+    }
+    return array;
+}
 
 function create_card(){
     console.log('create card');
     var game_area = $("#game-area");
-    for(var i = 0; i < 18; i++){
-        var create_card_div = $("<div>").addClass('card no' + i);
-        var create_front_div = $("<div>").addClass('front');
-        var create_front_img = $("<img>").attr({'src': game_images[i][0], 'alt': game_images[i][1]});
-        var create_back_div = $("<div>").addClass('back');
-        var create_back_img = $("<img>").attr({'src': 'images/Cat-Back.png', 'alt': 'card back'});
-        create_front_div.append(create_front_img);
-        create_back_div.append(create_back_img);
-        create_card_div.append(create_front_div, create_back_div);
-        game_area.append(create_card_div);
+    var consecutive_index = [1, 10];
+    for(var card_pair = 0; card_pair < 2; card_pair++) {
+        random_array(game_images);
+        for (var index = 0; index < game_images.length; index++) {
+            var create_card_div = $("<div>").addClass('card no' + (index + consecutive_index[card_pair]));
+            var create_front_div = $("<div>").addClass('front');
+            var create_front_img = $("<img>").attr({
+                'src': game_images[index][0],
+                'alt': game_images[index][1]
+            });
+            var create_back_div = $("<div>").addClass('back');
+            var create_back_img = $("<img>").attr({
+                'src': 'images/galaxy.png',
+                'alt': 'card back'
+            });
+            create_front_div.append(create_front_img);
+            create_back_div.append(create_back_img);
+            create_card_div.append(create_front_div, create_back_div);
+            game_area.append(create_card_div);
+        }
     }
-
 }
 
 function card_clicked(){
@@ -71,8 +73,9 @@ function card_clicked(){
             first_card_clicked = $(this); //true: assign first_card_clicked = html DOM Element that was clicked
             first_card_details.class = $(this).attr('class');
             first_card_details.imgsource = $(".front img", this).attr('src');
-            disable_card(first_card_clicked);
+            //$(".card", this).off('click');
             console.log(first_card_details);
+            console.log(first_card_clicked);
         } else {
             second_card_clicked = $(this); //false: assign second_card_clicked = html DOM Element that was clicked
             second_card_details.class = $(this).attr('class');
@@ -82,7 +85,7 @@ function card_clicked(){
             console.log(wait_timeout);
             if (first_card_details.class !== second_card_details.class && first_card_details.imgsource === second_card_details.imgsource) { //check if first_card_clicked === second_card_clicked
                 console.log('correct match');
-                disable_card(second_card_clicked); //###remove click event from success matches
+                //$(".card", this).off('click'); //###remove click event from success matches
                 match_counter++; //true: increment match_counter by 1
                 first_card_clicked = null; //reset both variables back to null
                 second_card_clicked = null;
@@ -94,7 +97,7 @@ function card_clicked(){
                 } //false: click handler function is complete, return
             } else {
                 console.log('not matched');
-                setTimeout(reset_incorrect_match, 2000); //false: wait 2s, then perform a function
+                setTimeout(reset_incorrect_match, 500); //false: wait 2s, then perform a function
             }
         }
     }
@@ -115,14 +118,10 @@ function reset_incorrect_match(){
     console.log('reset cards');
     $(".back", first_card_clicked).show();
     $(".back", second_card_clicked).show(); //show card back on both elements that are flipped over
-    $(first_card_clicked).on('click', card_clicked);
+    //$(first_card_clicked).on('click', card_clicked);
     first_card_clicked = null; //reset both card_clicked variables back to null
     second_card_clicked = null;
     first_card_details = {};
     second_card_details = {};
     wait_timeout = false;
-}
-
-function disable_card(card){
-    $(card).off('click');
 }
