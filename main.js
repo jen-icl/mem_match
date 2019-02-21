edd394e137c06b0bf30623793d12578eb0851d98
I Chun Lai <jenniferlai.81@gmail.com>
2019-02-20 18:12:39 -0800

    dom creation of cards

--- main.js
+++ main.js
@@ -2,7 +2,8 @@ $(document).ready(initializeApp);
 
 function initializeApp(){
     console.log('ready');
-    $(".card").on('click', card_clicked);
+    create_card();
+    $("#game-area").on('click', '.card', card_clicked);
 }
 
 var first_card_clicked = null;
@@ -13,11 +14,60 @@ var wait_timeout = false;
 var total_possible_matches = 2; //full game = 9
 var match_counter = 0;
 
+/*
+<div class="card happy1">
+    <div class="front">
+        <img src="images/Cat-Happy.png" alt="image-front">
+    </div>
+    <div class="back">
+        <img src="images/Cat-Back.png" alt="image-back">
+    </div>
+</div>
+ */
+var game_images = [
+    ['images/Cat-Happy.png', 'happy'],
+    ['images/Cat-Sad.png', 'sad'],
+    ['images/Cat-Happy.png', 'happy'],
+    ['images/Cat-Sad.png', 'sad'],
+    ['images/Cat-Happy.png', 'happy'],
+    ['images/Cat-Sad.png', 'sad'],
+    ['images/Cat-Happy.png', 'happy'],
+    ['images/Cat-Sad.png', 'sad'],
+    ['images/Cat-Happy.png', 'happy'],
+    ['images/Cat-Sad.png', 'sad'],
+    ['images/Cat-Happy.png', 'happy'],
+    ['images/Cat-Sad.png', 'sad'],
+    ['images/Cat-Happy.png', 'happy'],
+    ['images/Cat-Sad.png', 'sad'],
+    ['images/Cat-Happy.png', 'happy'],
+    ['images/Cat-Sad.png', 'sad'],
+    ['images/Cat-Happy.png', 'happy'],
+    ['images/Cat-Sad.png', 'sad'],
+
+]; //image source code list
+
+function create_card(){
+    console.log('create card');
+    var game_area = $("#game-area");
+    for(var i = 0; i < 18; i++){
+        var create_card_div = $("<div>").addClass('card no' + i);
+        var create_front_div = $("<div>").addClass('front');
+        var create_front_img = $("<img>").attr({'src': game_images[i][0], 'alt': game_images[i][1]});
+        var create_back_div = $("<div>").addClass('back');
+        var create_back_img = $("<img>").attr({'src': 'images/Cat-Back.png', 'alt': 'card back'});
+        create_front_div.append(create_front_img);
+        create_back_div.append(create_back_img);
+        create_card_div.append(create_front_div, create_back_div);
+        game_area.append(create_card_div);
+    }
+
+}
+
 function card_clicked(){
     console.log('enter card_clicked');
     if(!wait_timeout) {
         $(".back", this).hide(); //show card face or $(this).find(".back").hide();
-        if (first_card_clicked === null) { //check if first_card_clicked === null
+        if (first_card_clicked === null) {  //check if first_card_clicked === null
             first_card_clicked = $(this); //true: assign first_card_clicked = html DOM Element that was clicked
             first_card_details.class = $(this).attr('class');
             first_card_details.imgsource = $(".front img", this).attr('src');
@@ -50,12 +100,14 @@ function card_clicked(){
     }
 }
 //v put the check class and imgsource in an object
-//toggle function .... double-clicking first card will unflip the card - think of how to inactivate the first card until second card is clicked
+//v double-clicking first card will unflip the card - think of how to inactivate the first card until second card is clicked
 //v add a global flag (check_card_flipped = true)
 //v pointer-event: none to avoid card flipping during reset delay doesn't work
+
+//v DOM creation - create cards
+//shuffle cards - randomize  array - https://stackoverflow.com/questions/2450954/how-to-randomize-shuffle-a-javascript-array
+
 //add css to alert dialog
-//DOM creation - create cards
-//shuffle cards
 //.slideDown() jQuery for completion message
 //remove all console.log() after checks
 
@@ -63,7 +115,7 @@ function reset_incorrect_match(){
     console.log('reset cards');
     $(".back", first_card_clicked).show();
     $(".back", second_card_clicked).show(); //show card back on both elements that are flipped over
-    reactivate_card(first_card_clicked);
+    $(first_card_clicked).on('click', card_clicked);
     first_card_clicked = null; //reset both card_clicked variables back to null
     second_card_clicked = null;
     first_card_details = {};
@@ -73,8 +125,4 @@ function reset_incorrect_match(){
 
 function disable_card(card){
     $(card).off('click');
-}
-
-function reactivate_card(card){
-    $(card).on('click', card_clicked);
 }
